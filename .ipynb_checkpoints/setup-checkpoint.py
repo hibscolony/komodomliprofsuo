0554393e51818b4da3b5c8cbf94cda9606b58c72
Hanif Imaduddin <hanifimaduddin2@gmail.com>
2025-04-30 22:08:26 +0700

    Menambahkan parameter static_params

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="komodomliprofsuo",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.20.0",
+        "scikit-learn>=1.0.0",
+    ],
+    author="Nama Anda",
+    description="Komodo Mlipir Algorithm for Hyperparameter Optimization",
+    url="https://github.com/hibscolony/komodomliprofsuo",
+)
